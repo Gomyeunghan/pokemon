7e70e87e66a3deb6c9cf7bcd8ebcb237d7242d50
Gomyeunghan <rhaudgks04@gmail.com>
2025-02-24 16:04:21 +0900

    트레킹 test

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,7 @@ import type { NextPage } from "next";
 import type { ReactElement, ReactNode } from "react";
 import "@/styles/globals.css";
 import "@/styles/icons.css";
+import Script from "next/script";
 
 type NextPageWithLayout = NextPage & {
   getLayout?: (page: ReactElement) => ReactNode;
@@ -14,5 +15,21 @@ type appPropsWithLayout = AppProps & {
 
 export default function App({ Component, pageProps }: appPropsWithLayout) {
   const getLayout = Component.getLayout ?? ((page) => page);
-  return getLayout(<Component {...pageProps} />);
+  const projectId = "qeprkax4y8";
+  return getLayout(
+    <>
+      <Script
+        id="clarity-script"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `(function(c,l,a,r,i,t,y){
+          c[a] = c[a] || function(){(c[a].q=c[a].q||[]).push(arguments)};
+          t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+          y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+      })(window, document, "clarity", "script", "${projectId}");`,
+        }}
+      />
+      <Component {...pageProps} />{" "}
+    </>
+  );
 }
